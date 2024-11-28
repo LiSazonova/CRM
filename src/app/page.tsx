3b62dffa54c44698bfed177a811e4f587aa0a1c2
Lili Sazonova <lilissazonova@gmail.com>
2024-11-28 14:32:14 +0200

    eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { headers } from 'next/headers';
 import StatusLabel from './components/status-label';
 import { Status } from './components/status-label';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Welcome to CRM</h1>
+      <h1 className="text-xl">Welcome to CRM {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
