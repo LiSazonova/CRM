df34e6c2728099df27e78fdb3b292a52f65e7012
Lili Sazonova <lilissazonova@gmail.com>
2024-11-28 15:57:56 +0200

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import { headers } from 'next/headers';
-import StatusLabel from './components/status-label';
-import { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
   console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Welcome to CRM {new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Welcome to CRM</h1>
+      <AddCompanyButton />
     </main>
   );
 }
