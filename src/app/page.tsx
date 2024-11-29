30f42d202d9a9a088a30b559d73d199cc592686a
Lili Sazonova <lilissazonova@gmail.com>
2024-11-29 17:17:27 +0200

    add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { headers } from 'next/headers';
-import AddCompanyButton from './components/add-company-button';
-
 export default function Home() {
-  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Welcome to CRM</h1>
-      <AddCompanyButton />
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
